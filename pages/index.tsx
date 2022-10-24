432bfe1fd15b83ad6b05415dc92ff3f77dc7bc4e
ALT <andersonlottermann@gmail.com>
2022-10-24 01:38:02 -0300

    title

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to our site on <a href="https://fleek.co">IPFS!</a>
         </h1>
 
         <p className={styles.description}>
